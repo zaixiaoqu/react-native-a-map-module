f0f4943f3637b38780c967010d99a6c3e77bc596
余胜 <wx_a2f53f9499804ae094c7d8ca7817d653@git.code.tencent.com>
2019-12-13 13:12:32 +0800

    ios支持地图map页面显示

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { NativeModules } from 'react-native';
+import { MapView, Marker, Polyline, Polygon, Circle, HeatMap, MultiPoint, Offline } from './lib/amap3d';
 
 const { AMapModule } = NativeModules;
 
 export default AMapModule;
+export { MapView, Marker, Polyline, Polygon, Circle, HeatMap, MultiPoint, Offline };
